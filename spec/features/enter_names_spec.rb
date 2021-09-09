7f824a6c7c87b43b7071243c22542d97e1461ec0
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-09 16:21:50 +0100

    Add web_helpers.rb

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,7 @@
 feature 'Enter names' do
   scenario 'submitting names' do 
-    visit('/')
-    fill_in :player_1_name, with: 'Lilly'
-    fill_in :player_2_name, with: 'Fred'
-    click_button 'Submit'
-
+    sign_in_and_play
     save_and_open_page # will save the web page and open the browser to display it
-    
     expect(page).to have_content 'Lilly vs. Fred'
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'view hit points' do
   scenario 'see Player 2 hit points' do
-    visit("/")
-    fill_in :player_1_name, with: "Lilly"
-    fill_in :player_2_name, with: "Fred"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Fred: 60HP"
   end
 end
