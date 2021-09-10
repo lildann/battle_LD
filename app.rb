4f0c7abd36f2e33bc8487a1ae639609e2c02f969
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-10 16:36:14 +0100

    Extracting logic to a Player class

--- app.rb
+++ app.rb
@@ -10,20 +10,20 @@ class Battle < Sinatra::Base #loaded Sinatra into our controller
   end
 
   post '/names' do #post request gets redirected to get play
-    $player = params[:player_1_name]
-    $player = params[:player_2_name]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect :play
   end
 
   get '/play' do
-    $player[:player_1_name]
-    $player[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb :play
   end
 
   get '/attack' do 
-    $player[:player_1_name]
-    $player[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb :attack
   end 
 
